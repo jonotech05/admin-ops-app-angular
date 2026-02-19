bcdf52640b89a3ad0d5251fbf66d9626cdfd8ffb
jono nyams <jono@tech.io>
2026-02-18 20:46:17 -0800

    define and use input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello, world from JONO TECH!');
+import console from "console";
+
+import { getInput } from "@actions/core";
+
+const inputName = getInput("name");
+
+greet(inputName)
+
+function greet(name: string): void {
+  console.log(`'Hello, ${name} from JONO TECH!'`)
+}
